117f9d315609324895607f3e91259ed60b8c5e16
cytopia <cytopia@everythingcli.org>
2021-03-19 11:17:39 +0100

    More verbose docker logs

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -11,23 +11,22 @@ DEF_PHPIDS_VERBOSE="false"
 ### Summon env variables
 ###
 if env | grep '^RECAPTCHA_PRIV_KEY=' >/dev/null; then
-	>&2 echo  "Setting Recaptcha private key."
+	>&2 printf "Setting Recaptcha priv key: %s\\n" "$( echo "${RECAPTCHA_PRIV_KEY}" | sed 's/./*/g' )"
 	DEF_RECAPTCHA_PRIV_KEY="${RECAPTCHA_PRIV_KEY}"
 fi
 if env | grep '^RECAPTCHA_PUB_KEY=' >/dev/null; then
-	>&2 echo  "Setting Recaptcha public key."
+	>&2 printf "Setting Recaptcha pub key:  %s\\n" "$( echo "${RECAPTCHA_PUB_KEY}" | sed 's/./*/g' )"
 	DEF_RECAPTCHA_PUB_KEY="${RECAPTCHA_PUB_KEY}"
 fi
 if env | grep '^SECURITY_LEVEL=' >/dev/null; then
-	>&2 echo  "Setting Security level."
+	>&2 printf "Setting Security level:     %s\\n" "${SECURITY_LEVEL}"
 	DEF_SECURITY_LEVEL="${SECURITY_LEVEL}"
 fi
 if env | grep '^PHP_DISPLAY_ERRORS=' >/dev/null; then
-	>&2 echo  "Setting PHP error display."
+	>&2 printf "Setting PHP error display:  %s\\n" "${PHP_DISPLAY_ERRORS}"
 	DEF_PHP_DISPLAY_ERRORS="${PHP_DISPLAY_ERRORS}"
 fi
 if env | grep '^PHPIDS_ENABLED=' >/dev/null; then
-	>&2 echo  "Setting PHP IDS enabled/disabled."
 	DEF_PHPIDS_ENABLED="${PHPIDS_ENABLED}"
 	if [ "${PHPIDS_ENABLED}" = "0" ]; then
 		DEF_PHPIDS_ENABLED="disabled"
@@ -35,15 +34,17 @@ if env | grep '^PHPIDS_ENABLED=' >/dev/null; then
 	if [ "${PHPIDS_ENABLED}" = "1" ]; then
 		DEF_PHPIDS_ENABLED="enabled"
 	fi
+	>&2 printf "Setting PHP IDS state:      %s\\n" "${DEF_PHPIDS_ENABLED}"
+
 fi
 if env | grep '^PHPIDS_VERBOSE=' >/dev/null; then
-	>&2 echo  "Setting PHP IDS verbosity."
 	if [ "${PHPIDS_VERBOSE}" = "0" ]; then
 		DEF_PHPIDS_VERBOSE="false"
 	fi
 	if [ "${PHPIDS_VERBOSE}" = "1" ]; then
 		DEF_PHPIDS_VERBOSE="true"
 	fi
+	>&2 printf "Setting PHP IDS verbosity:  %s\\n" "${DEF_PHPIDS_VERBOSE}"
 fi
 
 
